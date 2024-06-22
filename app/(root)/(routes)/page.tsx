e4691da45660c7d20d01dc5dbd55c6da60259dba
novanishant <nishantverma3633@gmail.com>
2024-06-22 23:34:10 +0530

    Companion List Created

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,14 +1,57 @@
 import SearchInput from "@/components/SearchInput";
 import prismadb from "@/lib/prismadb";
 import { Categories } from "@/components/categories";
-const RootPage = async () => {
+import Companions from "@/components/companions";
+
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  }
+}
+const RootPage = async ({ searchParams }: RootPageProps) => {
+  const { categoryId, name } = searchParams;
+
+  // Sanitize input
+  const sanitizedCategoryId = categoryId?.trim() || '';
+  const sanitizedName = name?.trim() || '';
+
+  // Construct query
+  const query: any = {
+    where: {},
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  };
+
+  if (sanitizedCategoryId) {
+    query.where.categoryId = sanitizedCategoryId;
+  }
+
+  if (sanitizedName) {
+    query.where.name = {
+      contains: sanitizedName, // Use 'contains' for partial match
+      mode: 'insensitive', // Optional: for case insensitive search
+    };
+  }
+
+  const data = await prismadb.companion.findMany(query);
+  
   const categories = await prismadb.category.findMany();
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
-      <Categories data={categories}/>
+      <Categories data={categories} />
+      <Companions data={data}/>
     </div>
-  ); 
+  );
 }
 
 export default RootPage;
